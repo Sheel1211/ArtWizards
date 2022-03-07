c30669771cdcacae401278cf36460a59e1421ae5
Sheel1211 <96932878+Sheel1211@users.noreply.github.com>
2022-03-07 22:35:04 +0530

    product_upload_form is complete

--- product_upload_form/product_upload_form.php
+++ product_upload_form/product_upload_form.php
@@ -2,39 +2,32 @@
 
 $error = "";
 
-if(isset($_POST['submit']))
-{
+if (isset($_POST['submit'])) {
 
-    if($_FILES['main_image'] != NULL && $_POST['product_name'] != NULL && $_POST['discription'] != NULL && $_POST['cancelled_price'] != NULL && $_POST['original_price'] != NULL)
-    {
+    if ($_FILES['main_image'] != NULL && $_POST['product_name'] != NULL && $_POST['description'] != NULL && $_POST['cancelled_price'] != NULL && $_POST['original_price'] != NULL) {
         $product_name = htmlspecialchars($_POST['product_name']);
-        $discription = htmlspecialchars($_POST['discription']);
+        $description = htmlspecialchars($_POST['description']);
         $cancelled_price = htmlspecialchars($_POST['cancelled_price']);
         $original_price = htmlspecialchars($_POST['original_price']);
 
         $product_name = trim($product_name);
-        $discription = trim($discription);
+        $description = trim($description);
         $cancelled_price = trim($cancelled_price);
         $original_price = trim($original_price);
 
         // validation to check whether uploaded file is IMAGE file or not
         $filename = $_FILES['main_image']['name'];
         $ext = pathinfo($filename, PATHINFO_EXTENSION);
-        if ($ext !== 'gif' || $ext !== 'png' || $ext !== 'jpg') 
-        {
+        if ($ext !== 'gif' || $ext !== 'png' || $ext !== 'jpg') {
             $error = "Only image files are allowed!";
         }
-
-    }
-
-    else
-    {
+    } else {
         $error = "All fields are necessary to fill!";
     }
 
 
 
-    $filename=$_FILES['main_image']['name'];
+    $filename = $_FILES['main_image']['name'];
     $filesize = $_FILES['main_image']['size'];
 
     echo "$filename <br>";
@@ -50,16 +43,16 @@ if(isset($_POST['submit']))
 //     $fileinfo = @getimagesize($_FILES["file-input"]["tmp_name"]);
 //     $width = $fileinfo[0];
 //     $height = $fileinfo[1];
-    
+
 //     $allowed_image_extension = array(
 //         "png",
 //         "jpg",
 //         "jpeg"
 //     );
-    
+
 //     // Get image file extension
 //     $file_extension = pathinfo($_FILES["file-input"]["name"], PATHINFO_EXTENSION);
-    
+
 //     // Validate file input to check if is not empty
 //     if (! file_exists($_FILES["file-input"]["tmp_name"])) {
 //         $response = array(
@@ -119,6 +112,7 @@ if(isset($_POST['submit']))
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -126,83 +120,104 @@ if(isset($_POST['submit']))
     <title>Product Upload Form</title>
     <style>
         <?php
-            include './product_upload_form.css'
+        include './product_upload_form.css'
         ?>
     </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-<body>
 
-<!-- adding error span to display errors -->
-<span id="errorspan">
-    <?php
-        if(isset($error))
-        {
+<body>
+<section class="h-100 bg-dark">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col">
+                    <div class="card card-registration my-4">
+                        <div class="row g-0">
+                            <div class="col-xl-6 d-none d-xl-block">
+                                <img src="./product_upload_form_img.jpg"
+                                    alt="Sample photo" class="img-fluid"
+                                    style="border-top-left-radius: .25rem; border-bottom-left-radius: .25rem;" />
+                            </div>
+                            <div class="col-xl-6">
+                                <div class="card-body p-md-5 text-black">
+                                    <h3 class="mb-5 text-uppercase">Upload Product Details</h3>
+    <!-- adding error span to display errors -->
+    <span id="errorspan">
+        <?php
+        if (isset($error)) {
             echo "$error";
         }
-    ?>
-</span>
+        ?>
+    </span>
 
 
-<!-- *************************form starts from here********************** -->
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method = "POST" enctype = "multipart/form-data">
-    <table>
-        <tr>
-            <td>
-                <label for="main_image">Upload image for the main display : </label>
-            </td>
-            <td>
-                <input type="file" name="main_image" id="main_image">
-            </td>
-        </tr>
+    <!-- *************************form starts from here********************** -->
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
+    <div class="row">
+                                        <div class="col-md-6 mb-4">
+                                            <div class="form-outline">
+                                                <input type="file" id="formFile"
+                                                    class="form-label form-control form-control-lg" />
+                                                <label class="form-label" for="main_image">Upload image for the main display :<sup>*</sup></label>
+                                            </div>
+                                        </div>
 
-        <tr>
-            <td>
-                <label for="product_name">Product Name : </label>
-            </td>
 
-            <td>
-                <input type="text" name="product_name" id="product_name">
-            </td>
-        </tr>
+                                        <div class="row">
+                                        <div class="col-md-6 mb-4">
+                                            <div class="form-outline">
+                                                <input type="text" id="product_name" name = "product_name"
+                                                    class="form-control form-control-lg" />
+                                                <label class="form-label" for="product_name">Product Name : <sup>*</sup></label>
+                                            </div>
+                                        </div>
 
-        <tr>
-            <td>
-                <label for="discription">Discription : </label>
-            </td>
 
-            <td>
-                <input type="text" name="discription" id="discription">
-            </td>
-        </tr>
+                                        <div class="row">
+                                        <div class="col-md-6 mb-4">
+                                            <div class="form-outline">
+                                                <input type="text" id="description" name = "description"
+                                                    class="form-control form-control-lg" />
+                                                <label class="form-label" for="description">Description : <sup>*</sup></label>
+                                            </div>
+                                        </div>
 
-        <tr>
-            <td>
-                <label for="cancelled_price">Cancled Price : </label>
-            </td>
 
-            <td>
-                <input type="number" name="cancelled_price" id="cancelled_price">
-            </td>
-        </tr>
+                                        <div class="row">
+                                        <div class="col-md-6 mb-4">
+                                            <div class="form-outline">
+                                                <input type="text" id="cancelled_price" name = "cancelled_price"
+                                                    class="form-control form-control-lg" />
+                                                <label class="form-label" for="cancelled_price">Cancelled Price : <sup>*</sup></label>
+                                            </div>
+                                        </div>
 
-        <tr>
-            <td>
-                <label for="original_price">Original Price : </label>
-            </td>
 
-            <td>
-                <input type="number" name="original_price" id="original_price">
-            </td>
-        </tr>
+                                        <div class="row">
+                                        <div class="col-md-6 mb-4">
+                                            <div class="form-outline">
+                                                <input type="text" id="original_price" name = "original_price"
+                                                    class="form-control form-control-lg" />
+                                                <label class="form-label" for="original_price">Original Price : <sup>*</sup></label>
+                                            </div>
+                                        </div>
+                                        
 
-        <tr>
-            <td colspan = "2">
-                <input type="submit" value="Submit" name = "submit">
-            </td>
-        </tr>
+                                        <div class="d-flex justify-content-end pt-3">
+                                        <button type="reset" class="btn btn-light btn-lg" id = "reset" name = "reset">Reset all</button>
+                                        <button type="submit" class="btn btn-warning btn-lg ms-2" id = "submit" name = "submit">Submit form</button>
+                                    </div>
 
-    </table>
+
+
+                                        
+       
+
+           
+
+        
     </form>
 
 </body>
+
 </html>
